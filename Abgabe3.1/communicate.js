f100ed8161a91f94033e0b3fc93a613869541ac9
alessa2001 <80951472+alessa2001@users.noreply.github.com>
2021-05-30 14:36:31 +0200

    lol

--- Abgabe3.1/communicate.js
+++ Abgabe3.1/communicate.js
@@ -1,16 +1,14 @@
 "use strict";
-// tslint:disable-next-line: typedef
 async function communicater() {
+    event.preventDefault();
     let formData = new FormData(document.forms[0]);
-    let url = "https://gis-example.herokuapp.com";
+    let url = "https://servertest123somussdasssein.herokuapp.com/";
     let query = new URLSearchParams(formData);
     url = url + "?" + query.toString();
-    console.log(url);
     communicate(url);
 }
 async function communicate(_url) {
     let response = await fetch(_url);
-    console.log("sd");
     console.log("Response", response);
 }
 //# sourceMappingURL=communicate.js.map
--- Abgabe3.1/communicate.ts
+++ Abgabe3.1/communicate.ts
@@ -1,19 +1,21 @@
-// tslint:disable-next-line: typedef
+
 async function communicater(){
-   
+    event.preventDefault();
 let formData: FormData = new FormData(document.forms[0]);
 
 
-let url: string = "https://gis-example.herokuapp.com";
+let url: string = "https://servertest123somussdasssein.herokuapp.com/";
 let query: URLSearchParams = new URLSearchParams(<any>formData);
 url = url + "?" + query.toString();
-console.log(url);
 
 communicate(url);
+
+
 }
+
 async function communicate(_url: RequestInfo): Promise<void> {
     let response: Response = await fetch(_url);
-    console.log("sd");
+    
     console.log("Response", response);
    
   }
