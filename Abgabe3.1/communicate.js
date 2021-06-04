122759a2d88bc19deea8ea01008ba51b1baa4fd0
alessa2001 <80951472+alessa2001@users.noreply.github.com>
2021-06-04 08:23:37 +0200

    ausgabe

--- Abgabe3.1/communicate.js
+++ Abgabe3.1/communicate.js
@@ -8,25 +8,7 @@ async function communicater() {
     console.log("Response", response);
     let inhalt = await response.text();
     console.log(inhalt);
-    /*event.preventDefault();
-let formData: FormData = new FormData(document.forms[0]);
-
-
-let url: string = "https://servertest123somussdasssein.herokuapp.com/";
-let query: URLSearchParams = new URLSearchParams(<any>formData);
-url = url + "?" + query.toString();
-
-communicate(url);
-
-
-}
-
-async function communicate(_url: RequestInfo): Promise<void> {
-    let response: Response = await fetch(_url);
-    
-    console.log("Response", response);
-   
-  }*/
+    document.getElementById("antwort").innerHTML = inhalt;
 }
 document.getElementById("abschicken").addEventListener("click", communicater);
 //# sourceMappingURL=communicate.js.map
--- Abgabe3.1/communicate.ts
+++ Abgabe3.1/communicate.ts
@@ -1,14 +1,14 @@
 
 async function communicater(): Promise<void> {
-    let formData: FormData = new FormData(document.forms[0]);
-    let url: string = "https://servertest123somussdasssein.herokuapp.com/";
-    let query: URLSearchParams = new URLSearchParams(<any>formData);
-    url = url + "?" + query.toString();
-    let response: Response = await fetch(url);
-    console.log("Response", response);
-    let inhalt: string = await response.text();
-    console.log(inhalt);
+  let formData: FormData = new FormData(document.forms[0]);
+  let url: string = "https://servertest123somussdasssein.herokuapp.com/";
+  let query: URLSearchParams = new URLSearchParams(<any>formData);
+  url = url + "?" + query.toString();
+  let response: Response = await fetch(url);
+  console.log("Response", response);
+  let inhalt: string = await response.text();
+  console.log(inhalt);
+  document.getElementById("antwort").innerHTML = inhalt;
 
-  
 }
 document.getElementById("abschicken").addEventListener("click", communicater);
